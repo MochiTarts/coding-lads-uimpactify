a5e8e11461fde2bd9c65fec15359c17aa807bb26
RarePep8 <alex.wong.utsc@gmail.com>
2020-10-10 01:57:32 -0400

    Added barebones landing page

--- frontend/uimpactify-web/src/App.js
+++ frontend/uimpactify-web/src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+import bubbleBackground from './img/double-bubble-outline.png';
+
+const styles = {
+  main: {
+      backgroundImage: `url(${bubbleBackground})`,
+      backgroundRepeat:  "repeat",
+      height: '100vh'
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.main}>
+      <BrowserRouter>
+        <Route path="/" component={Landing} /> 
+      </BrowserRouter>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ frontend/uimpactify-web/src/pages/Landing.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Navbar, Nav, NavbarBrand, NavItem, NavLink } from "shards-react";
+
+const styles = {
+    navText: {
+        color: 'white'
+    }
+  };
+
+class Landing extends React.Component {
+
+    render() {
+        return (
+            <Navbar type="dark" theme="dark" expand="md">
+                <NavbarBrand href="#">U-Impactify</NavbarBrand>
+                <Nav>
+                    <NavItem >
+                        <NavLink active style={styles.navText} href="#">
+                            About Us
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink style={styles.navText} href="#">Pricing</NavLink>
+                    </NavItem>
+                </Nav>
+             </Navbar>
+        )
+    }
+}
+export default Landing;
