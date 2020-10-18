82881b91ecd44eead9fc8332acd6764ef757c824
RarePep8 <alex.wong.utsc@gmail.com>
2020-10-18 16:40:02 -0400

    Hooked up sign up page to api

--- frontend/uimpactify-web/src/helpers/services/user-service.js
+++ frontend/uimpactify-web/src/helpers/services/user-service.js
@@ -1,10 +1,17 @@
 import axios from 'axios';
+const apiUrl = 'http://localhost:8080';
 
-const apiUrl = 'placeholder';
-
-export const signUp = (firstname, lastname, username, password) => {
-    // return axios.post(`${apiUrl}/signup`,
-    // {firstname, lastname, username, password});
+export const signUp = (firstName, lastName, username, password, age, role, socialInit) => {
+    var roleObj;
+    if(role === "impact_learner" || role === "impact_consultant") {
+        roleObj = {name: role};
+    }
+    var socialInitObj;
+    if(socialInit) {
+        socialInitObj = {name: socialInit};
+    }
+    return axios.post(`${apiUrl}/signup`,
+    {firstName, lastName, username, password, age, role: roleObj, socialInit: socialInitObj});
     // MOCKING API CALL FOR NOW
     return Promise.resolve({});
 }
