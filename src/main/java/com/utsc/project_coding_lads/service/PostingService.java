198eb6da3f11802467d165acb61081f33829bd2f
Simon Ha <simon.ha@mail.utoronto.ca>
2020-10-20 14:26:22 -0400

    Adding get all postings

--- src/main/java/com/utsc/project_coding_lads/service/PostingService.java
+++ src/main/java/com/utsc/project_coding_lads/service/PostingService.java
@@ -1,11 +1,14 @@
 package com.utsc.project_coding_lads.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.utsc.project_coding_lads.domain.Posting;
 import com.utsc.project_coding_lads.exception.ValidationFailedException;
 
 public interface PostingService {
+	
+	final String SERVICE_NAME = "postings";
 
 	public Posting savePosting(Posting posting) throws ValidationFailedException;
 	
@@ -18,4 +21,8 @@ public interface PostingService {
 	public Posting updatePosting(Posting posting) throws ValidationFailedException;
 	
 	public List<Posting> findAllPostingsByUserId(Integer userId) throws ValidationFailedException;
+	
+	public List<Posting> findAllPostingsByUserIdDate(Integer userId, LocalDateTime date) throws ValidationFailedException;
+	
+	public List<Posting> getAllPostings();
 }
