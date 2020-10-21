4e1e32ef2027cafe335a416d9cc9f82ab66cd67a
TimofeySmetanin <71346240+TimofeySmetanin@users.noreply.github.com>
2020-10-21 11:17:54 -0400

    [COL-26] <added a username exception>

---
+++ src/main/java/com/utsc/project_coding_lads/exception/UsernameNotFoundException.java
@@ -0,0 +1,14 @@
+package com.utsc.project_coding_lads.exception;
+
+public class UsernameNotFoundException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4205870074752725643L;
+
+	public UsernameNotFoundException(String message) {
+		super(message);
+	}
+	
+		
+}
--- src/main/java/com/utsc/project_coding_lads/security/SecurityConfig.java
+++ src/main/java/com/utsc/project_coding_lads/security/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.utsc.project_coding_lads.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 
 import com.utsc.project_coding_lads.domain.User;
 import com.utsc.project_coding_lads.exception.MissingInformationException;
+import com.utsc.project_coding_lads.exception.UsernameNotFoundException;
 import com.utsc.project_coding_lads.exception.WrongPasswordException;
 import com.utsc.project_coding_lads.repository.UserRepository;
 
@@ -14,7 +15,7 @@ public class SecurityConfig {
 	@Autowired
 	UserRepository userRepo;
 
-	public Integer authentication(User entrant) throws WrongPasswordException, MissingInformationException {
+	public Integer authentication(User entrant) throws WrongPasswordException, MissingInformationException, UsernameNotFoundException {
 		String eUsername = entrant.getUsername();
 		String hashedPassword = entrant.getHashedPassword();
 
