3324e2a5958c48e7f5d97a4854b51e07a43a7e4a
Henry Lu <kuante.lu@mail.utoronto.ca>
2020-10-22 21:30:18 +0800

    Done, just need to hook up to controller

---
+++ frontend/uimpactify-web/src/components/dashboard/EventItem.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row } from "shards-react";
+
+class EventItems extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Container className={`${this.props.className} event-item`}>
+        <Row>
+          <img
+            class="event-image"
+            src={
+              this.props.image
+                ? this.props.image
+                : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/1200px-Question_mark_%28black%29.svg.png"
+            }
+          />
+        </Row>
+        <Row>
+          <div class="event-title">
+            {this.props.title ? this.props.title : "Event"}
+          </div>
+        </Row>
+        {this.props.omittime ? null : (
+          <Row>
+            <div class="event-time">
+              {this.props.time ? this.props.time : "Time"}
+            </div>
+          </Row>
+        )}
+      </Container>
+    );
+  }
+}
+export default EventItems;
