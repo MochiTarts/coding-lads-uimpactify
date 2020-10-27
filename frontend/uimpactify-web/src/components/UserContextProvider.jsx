feaf7d6904a4b6e7084f5bc93bd7b8729483e4f5
RarePep8 <alex.wong.utsc@gmail.com>
2020-10-27 15:57:16 -0400

    Added fetching of user fields to context

--- frontend/uimpactify-web/src/components/UserContextProvider.jsx
+++ frontend/uimpactify-web/src/components/UserContextProvider.jsx
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
+import {getUser} from "../helpers/services/user-service";
 const { Provider, Consumer } = React.createContext();
 
 class UserContextProvider extends Component {
   constructor(props) {
     super(props)
     const storedAuthenticatedUserId = localStorage.getItem('authenticatedUserId');
+    const storedAuthenticatedUser = JSON.parse(localStorage.getItem('authenticatedUser'));
     this.state = {
-      userId: storedAuthenticatedUserId
+      userId: storedAuthenticatedUserId,
+      user: storedAuthenticatedUser,
     };
   }
   signOut = () => {
     localStorage.removeItem('authenticatedUserId');
-    this.setState({userId: null})
+    localStorage.removeItem('authenticatedUser');
+    this.setState({userId: null, user: null})
   }
   signIn = (authenticatedUserId) => {
     localStorage.setItem('authenticatedUserId', authenticatedUserId);   
     this.setState({userId:authenticatedUserId})
+    getUser(authenticatedUserId).then(
+      (resp) => {
+        localStorage.setItem('authenticatedUser', JSON.stringify(resp.data));
+        const storedAuthenticatedUser = JSON.parse(localStorage.getItem('authenticatedUser'));
+        console.log(storedAuthenticatedUser);   
+        this.setState({user:resp.data})
+      }
+    )
+
   }
   render() {
-    return <Provider value={{userId: this.state.userId, signOut: this.signOut, signIn: this.signIn}}>{this.props.children}</Provider>;
+    return <Provider value={{userId: this.state.userId, user: this.state.user, signOut: this.signOut, signIn: this.signIn}}>{this.props.children}</Provider>;
   }
 }
 
