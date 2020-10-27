f38da491af2bfabc77ff6cd046f8d330d806f9e7
MochiTarts <jenny100.yu@gmail.com>
2020-10-27 14:14:52 -0400

    Made addCourse method to ImpactLearnerService

--- src/main/java/com/utsc/project_coding_lads/service/ImpactLearnerService.java
+++ src/main/java/com/utsc/project_coding_lads/service/ImpactLearnerService.java
@@ -1,9 +1,12 @@
 package com.utsc.project_coding_lads.service;
 
+import com.utsc.project_coding_lads.domain.Course;
 import com.utsc.project_coding_lads.domain.ImpactLearner;
 
 public interface ImpactLearnerService {
 
 	public Integer storeImpactLearner(ImpactLearner impactLearner) throws Exception;
+	
+	public Course addCourse(Course course) throws Exception;
 	
 }
--- src/main/java/com/utsc/project_coding_lads/service/impl/ImpactLearnerServiceImpl.java
+++ src/main/java/com/utsc/project_coding_lads/service/impl/ImpactLearnerServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.utsc.project_coding_lads.domain.Course;
 import com.utsc.project_coding_lads.domain.ImpactLearner;
 import com.utsc.project_coding_lads.repository.ImpactLearnerRepository;
 import com.utsc.project_coding_lads.service.ImpactLearnerService;
@@ -18,6 +19,12 @@ public class ImpactLearnerServiceImpl implements ImpactLearnerService {
 	@Override
 	public Integer storeImpactLearner(ImpactLearner impactLearner) throws Exception {
 		return impactLearnerRepo.save(impactLearner).getId();
+	}
+
+	@Override
+	public Course addCourse(Course course) throws Exception {
+
+		return null;
 	}
 
 }
