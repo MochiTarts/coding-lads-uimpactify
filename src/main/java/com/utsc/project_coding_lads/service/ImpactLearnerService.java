caa61cb08b1ed987b2c943b2ea747a4608be1539
MochiTarts <jenny100.yu@gmail.com>
2020-10-28 20:24:37 -0400

    Added methods for adding, removing, and retrieving courses for a learner

--- src/main/java/com/utsc/project_coding_lads/service/ImpactLearnerService.java
+++ src/main/java/com/utsc/project_coding_lads/service/ImpactLearnerService.java
@@ -1,12 +1,19 @@
 package com.utsc.project_coding_lads.service;
 
+import java.util.List;
+
 import com.utsc.project_coding_lads.domain.Course;
 import com.utsc.project_coding_lads.domain.ImpactLearner;
+import com.utsc.project_coding_lads.domain.User;
 
 public interface ImpactLearnerService {
 
 	public Integer storeImpactLearner(ImpactLearner impactLearner) throws Exception;
 	
-	public Course addCourse(Course course) throws Exception;
+	public Course addCourse(Integer courseId) throws Exception;
+	
+	public void removeCourse(Integer courseId) throws Exception;
+	
+	public List<Course> getAllCoursesByLearner(ImpactLearner learnerId) throws Exception;
 	
 }
--- src/main/java/com/utsc/project_coding_lads/service/impl/ImpactLearnerServiceImpl.java
+++ src/main/java/com/utsc/project_coding_lads/service/impl/ImpactLearnerServiceImpl.java
@@ -1,11 +1,14 @@
 package com.utsc.project_coding_lads.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.utsc.project_coding_lads.domain.Course;
 import com.utsc.project_coding_lads.domain.ImpactLearner;
+import com.utsc.project_coding_lads.domain.User;
 import com.utsc.project_coding_lads.repository.ImpactLearnerRepository;
 import com.utsc.project_coding_lads.service.ImpactLearnerService;
 
@@ -22,8 +25,20 @@ public class ImpactLearnerServiceImpl implements ImpactLearnerService {
 	}
 
 	@Override
-	public Course addCourse(Course course) throws Exception {
+	public Course addCourse(Integer courseId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public void removeCourse(Integer courseId) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Course> getAllCoursesByLearner(ImpactLearner learnerId) throws Exception {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
