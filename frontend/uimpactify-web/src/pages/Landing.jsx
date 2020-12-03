858da3e9a985470ae69ebf5eefa948b46bd65837
Henry <kuante.lu@mail.utoronto.ca>
2020-12-04 01:03:46 +0800

    Done

--- frontend/uimpactify-web/src/pages/Landing.jsx
+++ frontend/uimpactify-web/src/pages/Landing.jsx
@@ -4,7 +4,28 @@ import "../stylesheets/css/Landing.css";
 class Landing extends React.Component {
   render() {
     return (
-      <div></div>
+      <div className="about-section">
+        <div className="about-text">
+          <h5>What is Uimpactify?</h5>
+          <h2>
+            An online learning platform for scoial entrepreneurs and
+            intrapreneurs
+          </h2>
+          <div
+            className="get-started"
+            onClick={() => {
+              window.location.href = "/signup";
+            }}
+          >
+            GET STARTED
+          </div>
+        </div>
+        <img
+          className="about-image"
+          src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.clipartpanda.com%2Feducation-clip-art-yikL9r6iE.jpeg&f=1&nofb=1"
+          alt="land page pic"
+        />
+      </div>
     );
   }
 }
